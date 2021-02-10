94b3ca3643077d39b8358e4b646211f1a5e78c10
kassymkhan0101 <45078668+kassymkhan0101@users.noreply.github.com>
2021-02-10 14:43:48 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -9,6 +9,34 @@ const config7 = [['(', ')'], ['[', ']'], ['{', '}'], ['|', '|']];
     let newstr = str.split("");
     if(newstr % 2 == 1){
         return false;
+    }else{
+        let a = 2;
+        let b = 2;
+        let c = 2;
+        let d = 2;
+        let e = 2;
+        let f = 2;
+        let g = 2;
+    for(i=0; i<newstr.length; i++){
+        if (newstr[i] == '('){
+            a++
+        }else if (newstr[i] == ')'){
+            b++
+        }else if (newstr[i] == '['){
+            c++
+        }else if (newstr[i] == ']'){
+            d++
+        }else if (newstr[i] == '{'){
+            e++
+        }else if (newstr[i] == '}'){
+            f++
+        }else if (newstr[i] == '|'){
+            g++
+        }
+    }
+        if(a!=b || c!=d || e!=f || g%2==1){
+            return false;
+        }
     }
     
     
