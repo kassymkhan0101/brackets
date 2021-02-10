680dfcd2423948e0d1365a707186d5d1f60f877f
kassymkhan0101 <45078668+kassymkhan0101@users.noreply.github.com>
2021-02-10 13:59:36 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -8,18 +8,11 @@ const config6 = [['1', '2'], ['3', '4'], ['5', '6'], ['7', '7'], ['8', '8']];
 const config7 = [['(', ')'], ['[', ']'], ['{', '}'], ['|', '|']];
     let newstr = str.split("");
     
-    if(bracketsConfig == config1){
+ 
         for(i=0; i<newstr.length; i++){
-            let c = 0;
-            let k = 0;
-            if(newstr[i]== config1[0][0]){
-                c++;
-                console.log(c);
-            }else if(newstr[i]== config1[0][1]){
-                k++;
-                console.log(k)
-                     }
-        }
+            console.log(newstr[i]);
+           
+         
         
     }
     
