86a8785b5b5aad164b674daafcba150c43d7f6b0
kassymkhan0101 <45078668+kassymkhan0101@users.noreply.github.com>
2021-02-16 15:30:00 +0600

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,18 +3,20 @@ module.exports = function check(str, bracketsConfig) {
     if(str.length % 2 == 1){
         return false;
     }
-    for(i=0; i<bracketConfig.length; i++){
+    for(i=0; i<bracketsConfig.length; i++){
         for(k=0; k<i.length; k++){
             if(bracket[i][k]%2==1){
-                obj.bracket[i][k]=(i+1)
+                obj.bracketsConfig[i][k]=-(i+1)
             }else{
-                obj.bracket[i][k]= - (i+1)
+                obj.bracketsConfig[i][k]= (i+1)
             }
         }
     }
     for(j=0; j<str.length-1; j++){
         if((obj[str.charAt(j)] + obj[str.charAt(j+1)]) < 0){
             return false;
+        }else{
+            return true;
         }
     }
     
